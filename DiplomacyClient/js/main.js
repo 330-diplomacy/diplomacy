6e4473ca6cadc96277139ec86e51aabd8dedfae0
Dave Lee <camperdave614@gmail.com>
2012-11-10 15:31:10 -0600

    Killed unneeded js files, removed lib folder for easier use of require.js, added php debug demo and ensured that require.js works with both jquery and jquery ui

--- DiplomacyClient/js/main.js
+++ DiplomacyClient/js/main.js
@@ -1,9 +1,10 @@
 ﻿/// <reference path="lib/require-jquery.intellisense.js" />
 /// <reference path="lib/jquery-1.8.2.intellisense.js" />
 
-require(["jquery"], function ($) {
+require(["jquery", "jquery-ui-1.9.0"], function ($) {
 
     $(document).ready(function() {
         $("#test").append('<p>Hello from require-jquery and main!</p>');
+        $("#dialogTest").dialog();
     });
 });
--- DiplomacyServer/index.php
+++ DiplomacyServer/index.php
@@ -2,4 +2,12 @@
 
 echo "Hello World!";
 
+$length = 10;
+
+for ($i = 0; $i < $length; $i++)
+{
+    echo "count: $i";
+}
+
+
 ?>
