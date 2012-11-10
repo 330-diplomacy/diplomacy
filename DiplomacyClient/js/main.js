ae221856f4cbc8d3e087690c2917328b26f7d015
Dave Lee <camperdave614@gmail.com>
2012-11-10 15:16:51 -0600

    hacked up the intellisense jquery files a bit, also got my configuration working, php debugging is go.

---
+++ DiplomacyClient/js/main.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="lib/require-jquery.intellisense.js" />
+/// <reference path="lib/jquery-1.8.2.intellisense.js" />
+
+require(["jquery"], function ($) {
+
+    $(document).ready(function() {
+        $("#test").append('<p>Hello from require-jquery and main!</p>');
+    });
+});
