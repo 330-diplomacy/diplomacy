a1bd2f537b83cb3af3e11613afb32467e033414d
Dave Lee <camperdave614@gmail.com>
2012-11-12 21:54:29 -0600

    push to show css issue

---
+++ DiplomacyClient/js/game/Power.js
@@ -0,0 +1,19 @@
+﻿define('Power', function () {
+    // Start with the constructor
+    function Power(playerName, nationName, id, color) {
+        this.playerName = playerName;
+        this.nationName = nationName;
+        this.id = id;
+        this.color = color;
+    }
+
+    Power.loadPowers = function (powerList) {
+        var ret = [];
+        for (var i = 0; i < powerList.length; i++) {
+            ret.push(new Power(powerList[i]))
+        }
+    };
+
+    // And now return the constructor function
+    return Power;
+});
---
+++ DiplomacyClient/js/game/jquery-diplomacy.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../jquery-1.8.2.intellisense.js" />
+/// <reference path="../require-jquery.intellisense.js" />
+
+define(['jquery'], function ($) {
+    $.fn.diplomacyNavbar = function() {
+        // "this" references the navbar div.
+        
+    };
+});
