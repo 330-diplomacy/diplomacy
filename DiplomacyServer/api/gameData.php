bb2121436f5acfb1364126f6e95622225d3070e3
Mason Allen <masra91@gmail.com>
2012-11-14 02:35:45 -0600

    anotherfix

--- DiplomacyServer/api/gameData.php
+++ DiplomacyServer/api/gameData.php
@@ -90,139 +90,140 @@ class unitInfo
     }
 }
 
+function getBoard($gameID)
+{
+    global $mysqli;
+    
+    $provlist = $mysqli->preapre("SELECT name, abrv, type FROM provinces WHERE gameid=?");
+    if(!$provlist)
+    {
+        $err = "Query Prep Failed: ";
+        $err .= $mysqli->error;
+        
+        echo($err);
+        header("HTTP/1.0 500 Internal Server Error gameData 1");
+        exit;
+    }
+    
+    $provlist->bind_param("i", $gameID);
+    $provlist->execute();
+    
+    $provlist->bind_result($name, $abrv, $type);
+    
+    $provinces;
+    
+    while($provlist->fetch())
+    {
+        $temp = new provInfo($name, $abrv, $type);
+        $provinces[] = $temp;
+    }
+    
+    $provlist->close();
+    
+    $unitlist = $mysqli->prepare("SELECT type, powerid, locationid FROM units WHERE gameid=?");
+    if(!$unitlist)
+    {
+        $err = "Query Prep Failed: ";
+        $err .= $mysqli->error;
+        
+        echo($err);
+        header("HTTP/1.0 500 Internal Server Error gameData 2");
+        exit;
+    }    
+    
+    $unitlist->bind_param("i", $gameID);
+    $unitlist->execute;
+    
+    $unitlist->bind_result($type, $power, $loc);
+    $units;
+    
+    while($unitlist->fetch())
+    {
+        $temp = new unitInfo($type, $power, $loc);
+        $units[] = $temp;
+    }
+    
+    return new board($provinces, $units);       
+}
+
+function getPlayers($gameID)
+{
+    global $mysqli;
+    
+    $playerInfo = $mysqli->prepare("SELECT user, powername, powernum, powercolor FROM powers WHERE gameid=?");
+    if(!$playerInfo)
+    {
+        $err = "Query Prep Failed: ";
+        $err .= $mysqli->error;
+        
+        echo($err);
+        header("HTTP/1.0 500 Internal Server Error gameData 5");
+        exit;
+    }   
+    $playerInfo->bind_param("i", $gameID);
+    $playerInfo->execute();
+    
+    $playerInfo->bind_result($user, $powername, $powernum, $color);
+    
+    $players;
+    
+    while($playerInfo->fetch())
+    {
+        $temp = new player($user, $powername, $powernum, $color);
+        $players[] = $temp;
+    }
+    
+    return new playerslist($players);        
+}
+
+function getState($gameID)
+{
+    global $mysqli;
+    
+    $info = $mysqli->prepare("SELECT name, phase, variant FROM games WHERE id=?");
+    if(!$info)
+    {
+        $err = "Query Prep Failed: ";
+        $err .= $mysqli->error;
+        
+        echo($err);
+        header("HTTP/1.0 500 Internal Server Error gameData 3");
+        exit;
+    }
+    
+    $info->bind_param("i", $gameID);
+    $info->execute();
+    
+    $info->bind_result($name, $phase, $variant);
+    $fetch();
+    
+    $variantData = $mysqli->prepare("SELECT path, x_dimension, y_dimension FROM variants WHERE name=?");
+    if(!$info)
+    {
+        $err = "Query Prep Failed: ";
+        $err .= $mysqli->error;
+        
+        echo($err);
+        header("HTTP/1.0 500 Internal Server Error gameData 4");
+        exit;
+    }
+    
+    $variantData->bind_param("s", $variant);
+    $variantData->execute();
+    
+    $variantData->bind_results($path, $xdim, $ydim);
+    $fetch();
+    
+    return new stateInfo($name, $phase, $path, $xdim, $ydim);
+}
+
+
 class gameInfo
 {
     public $boardInfo;
     public $playerList;
     public $state;
         
-    public function getBoard($gameID)
-    {
-        global $mysqli;
-        
-        $provlist = $mysqli->preapre("SELECT name, abrv, type FROM provinces WHERE gameid=?");
-        if(!$provlist)
-        {
-            $err = "Query Prep Failed: ";
-            $err .= $mysqli->error;
-            
-            echo($err);
-            header("HTTP/1.0 500 Internal Server Error gameData 1");
-            exit;
-        }
-        
-        $provlist->bind_param("i", $gameID);
-        $provlist->execute();
-        
-        $provlist->bind_result($name, $abrv, $type);
-        
-        $provinces;
-        
-        while($provlist->fetch())
-        {
-            $temp = new provInfo($name, $abrv, $type);
-            $provinces[] = $temp;
-        }
-        
-        $provlist->close();
-        
-        $unitlist = $mysqli->prepare("SELECT type, powerid, locationid FROM units WHERE gameid=?");
-        if(!$unitlist)
-        {
-            $err = "Query Prep Failed: ";
-            $err .= $mysqli->error;
-            
-            echo($err);
-            header("HTTP/1.0 500 Internal Server Error gameData 2");
-            exit;
-        }    
-        
-        $unitlist->bind_param("i", $gameID);
-        $unitlist->execute;
-        
-        $unitlist->bind_result($type, $power, $loc);
-        $units;
-        
-        while($unitlist->fetch())
-        {
-            $temp = new unitInfo($type, $power, $loc);
-            $units[] = $temp;
-        }
-        
-        return new board($provinces, $units);       
-    }
-    
-    public function getPlayers($gameID)
-    {
-        global $mysqli;
-        
-        $playerInfo = $mysqli->prepare("SELECT user, powername, powernum, powercolor FROM powers WHERE gameid=?");
-        if(!$playerInfo)
-        {
-            $err = "Query Prep Failed: ";
-            $err .= $mysqli->error;
-            
-            echo($err);
-            header("HTTP/1.0 500 Internal Server Error gameData 5");
-            exit;
-        }   
-        $playerInfo->bind_param("i", $gameID);
-        $playerInfo->execute();
-        
-        $playerInfo->bind_result($user, $powername, $powernum, $color);
-        
-        $players;
-        
-        while($playerInfo->fetch())
-        {
-            $temp = new player($user, $powername, $powernum, $color);
-            $players[] = $temp;
-        }
-        
-        return new playerslist($players);        
-    }
-    
-    public function getState($gameID)
-    {
-        global $mysqli;
-        
-        $info = $mysqli->prepare("SELECT name, phase, variant FROM games WHERE id=?");
-        if(!$info)
-        {
-            $err = "Query Prep Failed: ";
-            $err .= $mysqli->error;
-            
-            echo($err);
-            header("HTTP/1.0 500 Internal Server Error gameData 3");
-            exit;
-        }
-        
-        $info->bind_param("i", $gameID);
-        $info->execute();
-        
-        $info->bind_result($name, $phase, $variant);
-        $fetch();
-        
-        $variantData = $mysqli->prepare("SELECT path, x_dimension, y_dimension FROM variants WHERE name=?");
-        if(!$info)
-        {
-            $err = "Query Prep Failed: ";
-            $err .= $mysqli->error;
-            
-            echo($err);
-            header("HTTP/1.0 500 Internal Server Error gameData 4");
-            exit;
-        }
-        
-        $variantData->bind_param("s", $variant);
-        $variantData->execute();
-        
-        $variantData->bind_results($path, $xdim, $ydim);
-        $fetch();
-        
-        return new stateInfo($name, $phase, $path, $xdim, $ydim);
-    }
-    
     public function __construct($GID)
     {
         $this->boardInfo = getBoard($GID);
