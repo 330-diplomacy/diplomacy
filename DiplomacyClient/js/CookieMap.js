cfc80285cf2138f29be4b15a4a64e36e87654641
Dave Lee <camperdave614@gmail.com>
2012-11-16 15:58:22 -0600

    properly send login data

--- DiplomacyClient/js/CookieMap.js
+++ DiplomacyClient/js/CookieMap.js
@@ -1,7 +1,7 @@
 ﻿// JScript File
 define(function() {
     return function(cookieString) {
-        var listOfCookies = cookieString.split(";");
+        var listOfCookies = cookieString.split("; ");
         var cookieMap = {};
         for (var i = 0; i < listOfCookies.length; i++) {
             var sCookie = listOfCookies[i].split('=');
