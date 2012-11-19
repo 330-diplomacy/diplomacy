38dc6f4bbd6cc6c7268785b2d0ef77e8874ede20
Mason Allen <masra91@gmail.com>
2012-11-19 03:12:12 -0600

    list updated.

---
+++ DiplomacyServer/api/game/list.php
@@ -0,0 +1,85 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/diplomacy/DiplomacyServer/resource/database.php");
+require_once("$root/diplomacy/DiplomacyServer/api/users/validate.php");
+
+class gameListData
+{
+    public $id;
+    public $name;
+    public $owner;
+    public $pcount;
+    public $max;
+    
+    public function __construct($id, $name, $owner, $pcount, $max)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->owner = $owner;
+        $this->pcount = $pcount;
+        $this->max = $max;
+    }
+}
+
+if(validate())
+{
+    $stmt = $mysqli->preapre("SELECT id, name, owner, player2, player3, player4, player5, player6, player7, variant, maxplayers FROM games");
+    if(!$stmt)
+    {
+		$err = "Query Prep Failed: ";
+		$err .= $mysqli->error;
+
+		echo($err);
+		header("HTTP/1.0 500 Internal Server Error List 1");
+		exit;
+	}
+    
+    $stmt->execute();
+    
+    $stmt->bind_results($id, $name, $owner, $player2, $player3, $player4, $player5, $player6, $player7, $variant, $max);
+    
+    $ans = array();
+    
+    while($stmt->fetch())
+    {
+        $pcount = '1'; 
+    
+        if(!is_null($player2))
+        {
+            $pcout++;
+        }
+        if(!is_null($player3))
+        {
+            $pcount++;
+        }
+        if(!is_null($player4))
+        {
+            $pcount++;
+        }
+        if(!is_null($player5))
+        {
+            $pcount++;
+        }
+        if(!is_null($player6))
+        {
+            $pcount++;
+        }
+        if(!is_null($player7))
+        {
+            $pcount++;
+        }
+    
+        $info = new gameListData($id, $name, $owner, $pcount, $max);
+        
+        $ans[] = $info;
+    }
+    
+    echo json_encode($ans);    
+}
+else
+{
+    
+}
+
+
+?>
