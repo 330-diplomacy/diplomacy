eb478622a9c936a770ea28ddee67c082e5b97dc2
Dave Lee <camperdave614@gmail.com>
2012-11-19 03:27:15 -0600

    units now display on map

--- DiplomacyClient/js/game/jquery-diplomacy.js
+++ DiplomacyClient/js/game/jquery-diplomacy.js
@@ -1,11 +1,11 @@
 ﻿/// <reference path="../jquery-1.8.2.intellisense.js" />
 /// <reference path="../require-jquery.intellisense.js" />
 
-define(['jquery', '../jquery-ui-1.9.0', 'game/DiplomacyGame'], function ($, jqueryUI, DiplomacyGame) {
+define(['jquery', '../jquery-ui-1.9.0', 'game/DiplomacyGame'], function($, jqueryUI, DiplomacyGame) {
 
     function onDiplomacyGameLoad(diplomacyGame) {
         // Each of these functions should probably do a this.each() - but we're not going to.
-        $.fn.diplomacyTopBar = function () {
+        $.fn.diplomacyTopBar = function() {
             // "this" references the navbar div.
             this.replaceWith(diplomacyGame.htmlTemplates.topBar);
 
@@ -16,42 +16,57 @@ define(['jquery', '../jquery-ui-1.9.0', 'game/DiplomacyGame'], function ($, jque
                 show: "blind",
                 hide: "explode"
             });
-            $("#topbarLoginLink").click(function () {
+            $("#topbarLoginLink").click(function() {
                 $("#topbarLoginDiv").dialog("open");
             });
         };
 
-        $.fn.diplomacyGameChat = function () {
+        $.fn.diplomacyGameChat = function() {
             // "this" references the chat div.
             this.replaceWith(diplomacyGame.htmlTemplates.gameChat);
 
             // Setup jquery on this div.
         };
 
-        $.fn.diplomacyMap = function () {
+        $.fn.diplomacyMap = function() {
             // "this" references the map div.
             this.replaceWith(diplomacyGame.htmlTemplates.gameMap);
 
             // Setup jquery on this div.
-            
+
             // First, create the province-label-abrv tag with the province name, and position them correctly.
-            $.each(diplomacyGame.mapData.provinces, function (index, value) {
+            $.each(diplomacyGame.mapData.provinces, function(index, value) {
                 var provinceId = 'province-label-' + value.abrv;
-                var provinceTitleHtmlString = '<p id="' + provinceId + '" class="province-label">' + value.name + '</p>';
+                var provinceDivId = 'province-label-div-' + value.abrv;
+                var provinceSpanId = 'province-label-span-' + value.abrv;
+                console.log(value.name, value.owner);
+                var provinceTitleHtmlString = '<div id="' + provinceDivId + '" class="province-label-div"><span id="' + provinceId + '" class="province-label">' + value.name + '</span></div>';
                 $("#map").append(provinceTitleHtmlString);
-                $('#' + provinceId).css("top", value.yloc);
-                $('#' + provinceId).css("left", value.xloc);
-                if (value.owner === -1) {
-                    $('#' + provinceId).css('background-color', 'white');
+                $('#' + provinceDivId).css("top", value.yloc);
+                $('#' + provinceDivId).css("left", value.xloc);
+                if (value.owner === null) {
+                    $('#' + provinceDivId).css('background-color', 'white');
                     $('#' + provinceId).css('color', 'black');
                 } else {
-                    $('#' + provinceId).css('background-color', 'darkgrey');
+                    $('#' + provinceDivId).css('background-color', 'darkgrey');
                     $('#' + provinceId).css('color', diplomacyGame.colorMap['P' + value.owner]);
                 }
+                if (value.width !== undefined || value.width !== null) {
+                    $('#' + provinceDivId).width(value.width);
+                }
             });
+
+            // Then, iterate through each unit, and show it on the province it lives in.
+            for (var mUI = 0; mUI < diplomacyGame.myUnits.length; mUI++) {
+                var mU = diplomacyGame.myUnits[mUI];
+                var unitType = (mU.type === 1) ? "Fleet" : "Army";
+                var unitHtmlString = '<span id="province-label-unit-%ABRV%" class="province-label-unit">%TYPE%</span>';
+                $('#province-label-div-' + mU.abrv).append(unitHtmlString.replace('%ABRV%', mU.abrv).replace('%TYPE%', unitType));
+                $("#province-label-unit-" + mU.abrv).css('color', diplomacyGame.colorMap['P' + mU.ownernum]);
+            }
         };
 
-        $.fn.diplomacyOrders = function () {
+        $.fn.diplomacyOrders = function() {
             // "this" references the orders div.
             this.replaceWith(diplomacyGame.htmlTemplates.gameOrders);
 
@@ -61,28 +76,25 @@ define(['jquery', '../jquery-ui-1.9.0', 'game/DiplomacyGame'], function ($, jque
                 var unitType = (mU.type === 1) ? "Fleet" : "Army";
                 var unitHtmlString = '<option id="order-unit-' + mU.location + '" class="order-unit value="' + mU.location + '" data-unitType="' + mU.type + '" data-location="' + mU.location + '">' + unitType + ' in ' + mU.location + '</option>';
                 $("#issueOrderUnit").append(unitHtmlString);
-                
+
             }
 
-            $.each(diplomacyGame.mapData.provinces, function (index, value) {
+            $.each(diplomacyGame.mapData.provinces, function(index, value) {
                 var destHtmlString = '<option id="order-dest-prov-' + value.abrv + '" class="order-dest-prov" value="' + value.abrv + '">' + value.name + '</option>';
                 $("#issueOrderDest").append(destHtmlString);
                 if (value.type === 0) {
                     $('#order-dest-prov-' + value.abrv).addClass("order-dest-prov-land");
-                }
-                else if (value.type === 1) {
+                } else if (value.type === 1) {
                     $('#order-dest-prov-' + value.abrv).addClass("order-dest-prov-coastal");
-                }
-                else if (value.type === 2) {
+                } else if (value.type === 2) {
                     $('#order-dest-prov-' + value.abrv).addClass("order-dest-prov-water");
-                }
-                else if (value.type === 3) {
+                } else if (value.type === 3) {
                     $('#order-dest-prov-' + value.abrv).addClass("order-dest-prov-two-coast");
                 }
             });
 
             var firstUnit = $('#issueOrderUnit > option').first();
-            
+
             if (firstUnit.attr("data-unitType") == "1")      // Fleet
             {
                 $(".order-dest-prov-water").removeAttr("disabled");
@@ -94,7 +106,7 @@ define(['jquery', '../jquery-ui-1.9.0', 'game/DiplomacyGame'], function ($, jque
                 $("#order-cmd-convoy").attr("disabled", "disabled");
             }
 
-            $("#issueOrderUnit").change(function () {
+            $("#issueOrderUnit").change(function() {
                 console.log("CHANGED UNIT");
                 console.log($("#issueOrderUnit:selected"));
                 if ($("#issueOrderUnit > option:selected").attr('data-unitType') == "1") {
