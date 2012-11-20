914ae05bc7ebad8084312ef36b03c815854556a1
Dave Lee <camperdave614@gmail.com>
2012-11-19 19:59:53 -0600

    create game / list my games 1

--- DiplomacyClient/index.php
+++ DiplomacyClient/index.php
@@ -13,6 +13,12 @@
             <div id="login-div"></div>
         </div>
         <div id="matches">
+            <button id="createNewGame">Create new game!</button>
+            <div title="Create a new Game!" id="createNewGameDiv">
+                <input id="newGameName" type="text" />
+                <input id="newGameVariant" type="text" value="standard" />
+                <button id="newGameSubmit">Create</button>
+            </div>
             <a href="game.php?gameID=7357">YOU JUST LOST THE GAME</a>
             <div id="my-match-div">
                 <ul id="my-match-list"></ul>
--- DiplomacyClient/js/index/fetchGames.js
+++ DiplomacyClient/js/index/fetchGames.js
@@ -14,5 +14,20 @@
         }).fail(function(errorData) {
             console.log("GAME LIST FAIL:", errorData);
         });
+        
+        $.ajax({
+            url: 'http://ec2-23-20-199-252.compute-1.amazonaws.com/diplomacy/DiplomacyServer/api/game/nyGames.php',
+            type: 'POST',
+            dataType: 'json',
+            data: loginDetails
+        }).done(function (data) {
+            $.event.trigger({
+                type: "diplomacy-myGameListLoaded",
+                message: data,
+                time: new Date()
+            });
+        }).fail(function (errorData) {
+            console.log("GAME LIST FAIL:", errorData);
+        });
     };
 });
